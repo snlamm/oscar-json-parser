6faadd1ff0fd9e256f948d1c9128c84d412657ba
snlamm <shmuel.lamm@gmail.com>
2016-07-05 17:59:01 -0400

    pseudocode method to extract winning film info for each year

--- app/adapters/data_grabber.rb
+++ app/adapters/data_grabber.rb
@@ -8,12 +8,17 @@ class DataGrabber
 
   def get_winners
     json_data = extract_json
-    binding.pry
-    # select only the winning films
+    extract_winners(json_data)
   end
 
   def extract_json
     response = RestClient.get(@url)
     JSON.parse(response)
+  end
+
+  def extract_winners(data)
+    # iterate through each film
+    # find which one is the winners
+    # extract the oscar year and the film url
   end
 end
