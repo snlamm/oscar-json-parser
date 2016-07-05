8aaecd5e8725c1b28dbde235c473ddf09fe2bfe6
snlamm <shmuel.lamm@gmail.com>
2016-07-05 17:47:27 -0400

    add pseudocode for building the oscar data map

--- app/services/oscar_data.rb
+++ app/services/oscar_data.rb
@@ -1,3 +1,14 @@
 class OscarData
-  
+  attr_accessor :root
+  def initialize(root)
+    @root = root
+    @oscar_map = []
+    build_map
+  end
+
+  def build_map
+    # get oscar winners for each year
+    # add winner's info to the oscar_map
+    # get budget average info
+  end
 end
