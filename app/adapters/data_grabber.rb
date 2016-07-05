f1c82647c33dcf372bce0c0ac1a716bfa9fe782b
snlamm <shmuel.lamm@gmail.com>
2016-07-05 17:51:51 -0400

    add data-grabber class and pseudocode get_winners method

---
+++ app/adapters/data_grabber.rb
@@ -0,0 +1,13 @@
+class DataGrabber
+
+  def initialize(url)
+    @url = url
+    @winners_list = []
+    get_winners
+  end
+
+  def get_winners
+    # extract data from the api
+    # select only the winning films
+  end
+end
