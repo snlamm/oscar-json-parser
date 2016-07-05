8115592404fbc3484573dda73a96d572e3dc37ec
snlamm <shmuel.lamm@gmail.com>
2016-07-05 18:13:38 -0400

    pseudocode method to extract data from winnerfilm class url

--- app/services/winner_film.rb
+++ app/services/winner_film.rb
@@ -1,7 +1,19 @@
 class WinnerFilm
 
-  attr_accessor :film_info
+  attr_accessor :film_info, :data_map
   def initialize(film_info)
     @film_info = film_info
+    @data_map = {}
+    parse_data
   end
+
+  def parse_data
+    # add the oscar year to data_map
+    # get raw json from the film link
+    #  add film title to data_map
+    # add release year to data_map
+    # add budget to data_map
+  end
+
+
 end
