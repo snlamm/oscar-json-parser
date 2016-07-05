c302d954d104b6bb27c02234a3c3b55d5c001bc1
snlamm <shmuel.lamm@gmail.com>
2016-07-05 18:25:27 -0400

    fix returned_data instance variable starting value

--- app/adapters/winner_data_grabber.rb
+++ app/adapters/winner_data_grabber.rb
@@ -1,3 +1,17 @@
 class WinnerDataGrabber
-  
+  attr_accessor :url, :returned_data
+  def initialize(url)
+    @url = url
+    @returned_data
+    get_data
+  end
+
+  def get_data
+    json_data = extract_json
+  end
+
+  def extract_json
+    response = RestClient.get(@url)
+    @returned_data = JSON.parse(response)
+  end
 end
