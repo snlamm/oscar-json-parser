bd2911862006066c7c12e8964acd947a0f9d4a18
snlamm <shmuel.lamm@gmail.com>
2016-07-05 17:48:34 -0400

    add get_winners method to extract winner info

--- app/services/oscar_data.rb
+++ app/services/oscar_data.rb
@@ -1,14 +1,20 @@
 class OscarData
-  attr_accessor :root
+  attr_accessor :root, :oscar_map, :winners
   def initialize(root)
     @root = root
     @oscar_map = []
+    @winners = []
     build_map
   end
 
   def build_map
-    # get oscar winners for each year
+    get_winners
     # add winner's info to the oscar_map
     # get budget average info
+  end
+
+  def get_winners
+    data = DataGrabber.new(@root)
+    @winners << data.winners_list
   end
 end
