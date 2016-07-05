e3eb3d5497660de4871d73889d62260d4d11f473
snlamm <shmuel.lamm@gmail.com>
2016-07-05 18:14:56 -0400

    add method to get oscar year from winner-film

--- app/services/winner_film.rb
+++ app/services/winner_film.rb
@@ -8,11 +8,17 @@ class WinnerFilm
   end
 
   def parse_data
-    # add the oscar year to data_map
+    get_oscar_year
     # get raw json from the film link
     #  add film title to data_map
     # add release year to data_map
     # add budget to data_map
+  end
+
+  def get_oscar_year
+    unformatted_year = @film_info[:year]
+    oscar_year = unformatted_year.match(/\d{4}/)[0]
+    @data_map[:oscar_year] = oscar_year
   end
 
 
