e926148325c0d2afb6c943e7759ffb1126f66a40
snlamm <shmuel.lamm@gmail.com>
2016-07-05 18:50:24 -0400

    pseudocode format_budget method

--- app/services/budget_formatter.rb
+++ app/services/budget_formatter.rb
@@ -1,3 +1,14 @@
 class BudgetFormatter
-  
+  attr_accessor :budget_string
+
+  def initialize(budget_string)
+    @budget_string = budget_string
+    format_budget
+  end
+
+  def format_budget
+    # use regex to parse budget string and extract match groups
+    # use recursion to go through match groups and take actions based on them
+    # set notices/errors if need be
+  end
 end
